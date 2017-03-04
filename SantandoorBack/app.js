6dac9d516a8eb48bfe2265c4f42d66a30e508489
D4n1 <daniel.garcia.garcia.84@gmail.com>
2017-03-04 19:13:26 +0100

    usuarios y reservas

--- SantandoorBack/app.js
+++ SantandoorBack/app.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/user', users);
 app.use('/building', buildings);
 app.use('/estimote', estimotes);
 
