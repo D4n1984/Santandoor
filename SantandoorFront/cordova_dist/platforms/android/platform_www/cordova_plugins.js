7881a7d75e1dc2add20c52b67bd316bbcd57d3a9
vlaraort <redteamwins@gmail.com>
2017-03-04 19:44:58 +0100

    notifications and beacons

--- SantandoorFront/cordova_dist/platforms/android/platform_www/cordova_plugins.js
+++ SantandoorFront/cordova_dist/platforms/android/platform_www/cordova_plugins.js
@@ -51,11 +51,54 @@ module.exports = [
         "runs": true
     },
     {
+        "id": "cordova-plugin-device.device",
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
         "id": "cordova-plugin-estimote.EstimoteBeacons",
         "file": "plugins/cordova-plugin-estimote/plugin/src/js/EstimoteBeacons.js",
         "pluginId": "cordova-plugin-estimote",
         "clobbers": [
             "estimote"
+        ]
+    },
+    {
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification",
+        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification.js",
+        "pluginId": "de.appplant.cordova.plugin.local-notification",
+        "clobbers": [
+            "cordova.plugins.notification.local",
+            "plugin.notification.local"
+        ]
+    },
+    {
+        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification.Core",
+        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification-core.js",
+        "pluginId": "de.appplant.cordova.plugin.local-notification",
+        "clobbers": [
+            "cordova.plugins.notification.local.core",
+            "plugin.notification.local.core"
+        ]
+    },
+    {
+        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification.Util",
+        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification-util.js",
+        "pluginId": "de.appplant.cordova.plugin.local-notification",
+        "merges": [
+            "cordova.plugins.notification.local.core",
+            "plugin.notification.local.core"
         ]
     }
 ];
@@ -63,7 +106,12 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "com.unarin.cordova.beacon": "3.4.1",
-    "cordova-plugin-estimote": "0.8.0"
+    "cordova-android-support-v4": "4.0.0",
+    "cordova-plugin-device": "1.1.4",
+    "cordova-plugin-estimote": "0.8.0",
+    "cordova-plugin-splashscreen": "4.0.1",
+    "cordova-plugin-whitelist": "1.3.1",
+    "de.appplant.cordova.plugin.local-notification": "0.8.2dev"
 };
 // BOTTOM OF METADATA
 });
