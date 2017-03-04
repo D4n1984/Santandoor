9163b98fe6bab3abac4dec39ed2d8410f47e2884
D4n1 <daniel.garcia.garcia.84@gmail.com>
2017-03-04 20:39:55 +0100

    open & close

--- SantandoorBack/routes/index.js
+++ SantandoorBack/routes/index.js
@@ -6,4 +6,34 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Another Consulting' });
 });
 
+router.post('/open', function(req, res, next){
+    var exec = require('child_process').exec,
+        child;
+
+    child = exec('"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe" C:\\Workspaces\\Santandoor\\SantandoorBack\\public\\images\\open.jpg',
+        function (error, stdout, stderr) {
+            console.log('Image opened');
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+        });
+
+    res.send('Abierto!');
+});
+
+router.post('/close', function(req, res, next){
+    var exec = require('child_process').exec,
+        child;
+
+    child = exec('"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe" C:\\Workspaces\\Santandoor\\SantandoorBack\\public\\images\\cerrado.jpg',
+        function (error, stdout, stderr) {
+            console.log('Image opened');
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+        });
+
+    res.send('Cerrado!');
+});
+
 module.exports = router;
